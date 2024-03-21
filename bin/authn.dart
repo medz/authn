70f37c680b31282221fd1e60fb505a720f3696f9
Seven Du <whine.whitish.0u@icloud.com>
2024-03-22 01:45:19 +0800

    chore: init

---
+++ bin/authn.dart
@@ -0,0 +1,5 @@
+import 'package:authn/authn.dart' as authn;
+
+void main(List<String> arguments) {
+  print('Hello world: ${authn.calculate()}!');
+}
---
+++ test/authn_test.dart
@@ -0,0 +1,8 @@
+import 'package:authn/authn.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
